c36c6389b5c51b67b2929692e131bd4f55607af7
Gavin Guinane <gguinane@gguinane-ltm.sfdcgomobile.salesforce.com>
2016-09-07 11:14:54 +0100

    Initial Commit

---
+++ Video/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Video
+//
+//  Created by Gavin Guinane on 9/7/16.
+//  Copyright © 2016 Gavin Guinane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Video/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Video
+//
+//  Created by Gavin Guinane on 9/7/16.
+//  Copyright © 2016 Gavin Guinane. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
